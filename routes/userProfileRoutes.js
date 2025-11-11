2a70b85cef0f393af52ffb5a095b8626937a613d
favour-udegbunem <favourudegbunem19@gmail.com>
2025-11-11 01:55:13 -0800

    I inserted an update and delete route

--- routes/userProfileRoutes.js
+++ routes/userProfileRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { upsertUserProfile, getUserProfile } from "../controllers/userProfileController.js";
+import { upsertUserProfile, getUserProfile, updateUserProfile, deleteUserProfile } from "../controllers/userProfileController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/userProfile", verifyToken, upsertUserProfile);
 router.get("/gettingUserProfile", verifyToken, getUserProfile);
+router.put("/update/id", verifyToken, getUserProfile);
+router.delete("/delete/id", verifyToken, getUserProfile);
 
 export default router;
